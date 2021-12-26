360c5b49ed7b287e834d4d30be8dba01a976bf28
bodarey <bodarey@yahoo.com>
2021-12-26 21:58:12 +0200

    ruby_game_strings

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -5,3 +5,7 @@ puts "#{player_name}'s health is #{health * 9}"
 puts "#{player_name}'s health is #{health.to_f/3}"
 puts "#{player_name}'s health is #{health/3}"
 puts "players are: \nLarry \nCurry\n Moe"
+
+
+#  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
+#  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
