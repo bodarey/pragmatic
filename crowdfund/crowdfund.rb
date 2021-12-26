46e386d9731aeec53a7672fc6b83061b08e4451e
bodarey <bodarey@yahoo.com>
2021-12-26 21:38:04 +0200

    first step

---
+++ crowdfund/crowdfund.rb
@@ -0,0 +1,6 @@
+puts "Project ABC has $1000 in funding."
+
+puts "Projects:\nProject ABC\nProject LMN\nProject XYZ"
+
+
+#    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
---
+++ studio_game/studio_game.rb
@@ -0,0 +1,7 @@
+player_name = "Larry"
+health = 60
+puts "#{player_name}'s health is #{health}"
+puts "#{player_name}'s health is #{health * 9}"
+puts "#{player_name}'s health is #{health.to_f/3}"
+puts "#{player_name}'s health is #{health/3}"
+puts "players are: \nLarry \nCurry\n Moe"
