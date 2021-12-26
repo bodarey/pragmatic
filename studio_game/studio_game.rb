c11f983b80e9d0d180fe92a8dc9f9048da7ea850
bodarey <bodarey@yahoo.com>
2021-12-27 00:09:02 +0200

    ruby_strings

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -5,7 +5,30 @@ puts "#{player_name}'s health is #{health * 9}"
 puts "#{player_name}'s health is #{health.to_f/3}"
 puts "#{player_name}'s health is #{health/3}"
 puts "players are: \nLarry \nCurry\n Moe"
+puts "#{player_name.capitalize}'s health is #{health}"
 
+player_2 = "currly"
+health_2 = 120
+puts 
+puts "#{player_2.upcase}'s health is #{health_2}"
+health_2 =  health
+puts "#{player_2.upcase}'s health is #{health_2}"
+health = 30
+puts "------------------"
+puts "#{player_name.capitalize}'s health is #{health}"
+puts "#{player_2.upcase}'s health is #{health_2}"
+
+player_3 = "moe"
+health_3 = 100
+puts "#{player_3.capitalize} has a health of #{health_3}".center(50,'*')
+
+player_4 = "shemp"
+health_4 = 90
+puts "#{player_4.capitalize}".ljust(30,'*') + "#{health_4} health"
+
+puts
+puts "-----------------"
+puts "the game started on #{Time.new.strftime("%A %m/%d/%Y at %I:%M%p ")}"
 
 #  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
 #  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
